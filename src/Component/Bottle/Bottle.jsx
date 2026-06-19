b7cd41231dd4da0b2c4655ad95066837487c3508
Nadimul Rahib <nadimulrahib38@gmail.com>
2026-06-19 22:49:30 +0600

    added bottle

--- src/Component/Bottle/Bottle.jsx
+++ src/Component/Bottle/Bottle.jsx
@@ -1,11 +1,12 @@
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handleBuy}) => {
     const { name, price, img} = bottle;
     return (
         <div className="bottle-card border p-4 rounded-lg shadow-md">
             <img src={img} alt={name} />
             <h1>{name}</h1>
             <p>Price: ${price}</p>
+            <button onClick={()=>handleBuy(bottle)} className="btn btn-primary">Buy Now</button>
         </div>
     );
 };
--- src/Component/Bottles/Bottles.jsx
+++ src/Component/Bottles/Bottles.jsx
@@ -1,7 +1,16 @@
-import  { use } from 'react';
+import  { use, useState } from 'react';
 import Bottle from '../Bottle/Bottle';
 
 const Bottles = ({ bottlesData }) => {
+
+    const [buy, setBuy] =useState([])
+
+    const handleBuy=(bottle)=>{
+
+        const addCart = [...buy, bottle]
+        setBuy(addCart)
+        console.log("add to cart", bottle)
+    }
 
     const bottles = use(bottlesData);
     console.log(bottles)
@@ -9,9 +18,10 @@ const Bottles = ({ bottlesData }) => {
     return (
         <div>
             <h1>Bottles : {bottles.length}</h1>
+            <p>add to cart: {buy.length}</p>
 
             <div className="bottles-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {bottles.map((bottle)=><Bottle key={bottle.id} bottle={bottle} />)}
+                {bottles.map((bottle)=><Bottle key={bottle.id} bottle={bottle} handleBuy={handleBuy}/>)}
             </div>
         </div>
     );
